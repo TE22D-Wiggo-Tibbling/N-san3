fce982137436e0d9ec5ff7c2bb0c73ea5683ae74
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-11-12 11:10:14 +0100

    aesfwetgewg

--- Näsan3/Program.cs
+++ Näsan3/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Steg 1
+using System.Runtime.InteropServices;
+
+void Hello32()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        System.Console.WriteLine("Hello World!");
+    }
+}
+
+// Steg 2
+
+void Integer(int kvadrat)
+{
+    Console.WriteLine(kvadrat * kvadrat);
+}
+
+// Steg 3
+
+float Multi(float float1, float float2)
+{
+    float product = float1 * float2;
+    return (product);
+}
+
+// Steg 4
+
+float RightTriangleArea(float hight, float width)
+{
+    float area = (hight * width) / 2;
+    return (area);
+}
+
+// Steg 5
+
+double CircleArea(double radius)
+{
+    double area = (radius * radius) * Math.PI;
+    return (area);
+}
+
+// Steg 6
+
+void GetNumber()
+{
+string awnser = "";
+while (true)
+{
+    
+}
+}
